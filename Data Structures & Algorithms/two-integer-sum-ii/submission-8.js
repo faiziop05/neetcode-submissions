2288c4e6320a0b8b9bc352d0762506a7ba449db0
Faizan Hanif <132082307+faiziop05@users.noreply.github.com>
2026-09-09 22:12:21 +0100

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let p1=0;
+        let p2=numbers.length-1
+        while(p1<p2){
+            let sum=numbers[p1]+numbers[p2]
+            if(sum===target) return [p1+1,p2+1]
+            else if(sum<target) p1++
+            else p2--
+        }
+        return []
+    }
+}
